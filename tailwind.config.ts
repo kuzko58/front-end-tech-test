554edea35f48a550c311f1277279103e29bb6443
kuzko58 <kuzko584@gmail.com>
2023-12-01 14:29:43 +0100

    added animations and changed naming conventions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
     theme: {
-        // ...defaultTheme,
         extend: {
             fontFamily: {
                 'satoshi-regular': [
